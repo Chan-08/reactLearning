6f31b915b9e6c61f1d31b0fe6babc58dec6964ce
Chandrakumar <itz.chandrakumar@gmail.com>
2025-10-09 12:22:43 +0530

    image import

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,10 @@
 import Welcome from "../components02/welcome"
+import Image from "../assets/react.svg"
 
 export default function About () {
     return <>
     <h1>About page</h1>
+    <img src={Image} alt="" />
     <Welcome/>
     </>
 }
